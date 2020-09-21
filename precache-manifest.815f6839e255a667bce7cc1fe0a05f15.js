2f4ea97f93d8f8710d9f432bbdd89d04bd8d0b0c
DoJunggeun <jg.do@snu.ac.kr>
2020-09-21 13:38:08 +0000

    Updates

---
+++ precache-manifest.815f6839e255a667bce7cc1fe0a05f15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29cbc51a7c6e15ce626e",
+    "url": "/chemian/static/js/runtime~main.29cbc51a.js"
+  },
+  {
+    "revision": "a671b72b1dce79656f61",
+    "url": "/chemian/static/js/main.a671b72b.chunk.js"
+  },
+  {
+    "revision": "a67d7e52567ccc0f5702",
+    "url": "/chemian/static/js/1.a67d7e52.chunk.js"
+  },
+  {
+    "revision": "a671b72b1dce79656f61",
+    "url": "/chemian/static/css/main.8d29088e.chunk.css"
+  },
+  {
+    "revision": "6f24ddf4b25daf1a4d6149d0e0175178",
+    "url": "/chemian/index.html"
+  }
+];
